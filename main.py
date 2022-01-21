b9e7e17ddef3ec962632a5651d1636cf4308db05
amon-wanyonyi <amon.wanyonyi@student.moringaschool.com>
2022-01-21 07:38:13 +0300

    First Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# Password Manager
+
+# Username
+username = str(input("Enter Username"))
+# Website
+
+# Generate Random Password
+
+# Hast the password
+
+# Store the password into a file
+
+#######
+
+# Search by username
+# search by website
