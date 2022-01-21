3e01f6d4b33e2f725613bf7c1878d39c93e0662b
amon-wanyonyi <amon.wanyonyi@student.moringaschool.com>
2022-01-21 09:10:13 +0300

    Add random password generator

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ import string
 
 def store_password():
     # Username
-    username = str(input("Enter Username: "))
+   username = str(input("Your username: "))
 
 
 # Website
 website = str(input("Enter Website: "))
 
 # Generate Random Password
-string.ascii_letters
+digit = int(input("How any digits do you want? (integer only)"))
+password = ""
+for i in range(digit):
+    char = random.choice(string.ascii_letters)
 
 
 # Hast the password
